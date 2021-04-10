da6d6efee56f7ee5d5c20f9e0b6725585a5559fc
faisal ali <faisalitengg@gmail.com>
2021-04-10 20:51:40 +0300

    Azure db connection

--- index.js
+++ index.js
@@ -7,13 +7,7 @@ const cors=require('cors');
 const bodyParser=require('body-parser'); //latest express has solution to it 
 
 const app=express();
-//app.use(cors());
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(cors());
 
 app.use(bodyParser.urlencoded({
     extended: true
