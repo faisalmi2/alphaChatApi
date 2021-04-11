d3de94cd2cd52af7ce92cc811ead0c7cff4c1cd2
Mir Faisal Ali <faisalitengg@gmail.com>
2021-04-11 01:40:17 -0700

    Cors options added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cors=require('cors');
 const bodyParser=require('body-parser'); //latest express has solution to it 
 
 const app=express();
+
 app.use(cors());
 
 app.use(bodyParser.urlencoded({
